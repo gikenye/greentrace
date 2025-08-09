867d83f8e8d452a638bbc368ab5cfd2722854032
Johnstone Gikenye <108178926+gikenye@users.noreply.github.com>
2025-08-09 17:08:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title:'mititrace',
+  description: 'seen a tree 🌳? Snap it, lets store it onchain',
+  generator: '0x',
 }
 
 export default function RootLayout({
